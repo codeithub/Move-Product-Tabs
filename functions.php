eda8fe410ba16956bf42151b4db260865f3068ee
Code IT Hub <78258092+codeithub@users.noreply.github.com>
2021-07-03 12:54:40 +0530

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,2 @@
+remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
+add_action( 'woocommerce_single_product_summary', 'woocommerce_output_product_data_tabs', 60 );
